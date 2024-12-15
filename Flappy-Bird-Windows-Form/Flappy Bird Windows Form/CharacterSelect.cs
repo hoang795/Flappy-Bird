f91235d3a0b3599c3768e0de0e961fef753b634a
hoang795 <ggwp240105@gmail.com>
2024-12-15 14:38:52 +0700

    Add files via upload

---
+++ Flappy-Bird-Windows-Form/Flappy Bird Windows Form/CharacterSelect.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flappy_Bird_Windows_Form
+{
+    public partial class CharacterSelect : Form
+    {
+        public CharacterSelect()
+        {
+            InitializeComponent();
+
+        }
+
+        private void CharacterSelect_Load(object sender, EventArgs e)
+        {
+            // Initialize any dynamic content or setup logic here.
+            MessageBox.Show(" Choose your character!");
+        }
+        private void Bird1_Click(object sender, EventArgs e)
+        {
+            OpenDifficultySelect("Bird1"); // Truyền thông tin nhân vật đã chọn
+        }
+
+        private void Bird2_Click(object sender, EventArgs e)
+        {
+            OpenDifficultySelect("Bird2"); // Truyền thông tin nhân vật đã chọn
+        }
+
+        private void OpenDifficultySelect(string selectedCharacter)
+        {
+            // Mở giao diện chọn độ khó và truyền nhân vật đã chọn
+            DifficultySelect difficultyForm = new DifficultySelect(selectedCharacter);
+            difficultyForm.Show();
+            this.Hide();
+        }
+    }
+}
