610ce1e38bf6fb66ee7f4680099dab617783daa8
hoang795 <ggwp240105@gmail.com>
2024-12-15 11:18:37 +0700

    Add files via upload

---
+++ Flappy-Bird-Windows-Form/Flappy Bird Windows Form/CharacterSelect.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flappy_Bird_Windows_Form
+{
+    public partial class CharacterSelect : Form
+    {
+        public CharacterSelect()
+        {
+            InitializeComponent();
+
+        }
+
+        private void CharacterSelect_Load(object sender, EventArgs e)
+        {
+            // Initialize any dynamic content or setup logic here.
+            MessageBox.Show(" Choose your character!");
+        }
+        private void Bird1_Click(object sender, EventArgs e)
+        {
+            StartGame("Normal");
+        }
+
+        private void Bird2_Click(object sender, EventArgs e)
+        {
+            StartGame("Normal");
+        }
+        private void StartGame(string mode)
+        {
+            Form1 gameForm = new Form1(mode);
+            gameForm.Show();
+            this.Hide();
+        }
+    }
+}
